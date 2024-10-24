b81ebc0cd2cb1f45caef8f65110c60b2a6ae6474
Justin Dosaj <jdosaj@vurge.io>
2024-10-24 14:08:51 -0700

    Publish Out Folder

---
+++ frontend/out/_next/static/xI-N0H7FVakcwmZ8UdmY4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-32831171231851ee.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],"/articles":["static/chunks/pages/articles-463b64506a2580b4.js"],sortedPages:["/","/_app","/_error","/articles"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
